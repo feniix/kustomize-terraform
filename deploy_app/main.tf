7f4b29f92217809828b07e1b77986629f799c848
Sebastian Otaegui <feniix@gmail.com>
2020-08-17 21:25:18 -0300

    add use of kustomization

--- deploy_app/main.tf
+++ deploy_app/main.tf
@@ -1,4 +1,9 @@
 module "kustomize" {
   source = "../modules/kustomize"
   kustomization_source = "../kustomize/base"
+
+  kustomization_image_repo = "docker.io/nginx"
+  kustomization_image_tag = "1.19.2-alpine"
+  kustomization_release_name = "myapp"
+  kustomization_namespace = "myapp"
 }
